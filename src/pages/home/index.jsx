7695e67730790b34c6dee9b582d08dcbafc6ccfe
bdbose <bdbose123@gmail.com>
2022-09-19 02:51:38 +0530

    hover link css change

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,6 +8,12 @@ import LinkedInLogo from '../../assets/svgs/linkedin.svg';
 import GithubLogo from '../../assets/svgs/github.svg';
 import TwitterLogo from '../../assets/svgs/twitter.svg';
 import YtLogo from '../../assets/svgs/youtube.svg';
+import FbLogoActive from '../../assets/svgs/facebook_active.png';
+import InstaLogoActive from '../../assets/svgs/instagram_active.png';
+import LinkedInLogoActive from '../../assets/svgs/linkedin_active.png';
+import GithubLogoActive from '../../assets/svgs/github_active.png';
+import TwitterLogoActive from '../../assets/svgs/twitter_active.png';
+import YtLogoActive from '../../assets/svgs/youtube_active.png';
 import { ReactComponent as LineSvg } from '../../assets/svgs/line.svg';
 import AboutMePhoto from '../../assets/svgs/about_me.svg';
 import { useEffect } from 'react';
@@ -20,27 +26,33 @@ import * as cube from '../../assets/svgs/cube.json';
 const SocialLinks = [
   {
     icon: LinkedInLogo,
-    link: '/',
+    active: LinkedInLogoActive,
+    link: 'https://www.linkedin.com/in/bidipto-bose-981b541b6/',
   },
   {
     icon: GithubLogo,
-    link: '/',
+    link: 'https://github.com/bdbose',
+    active: GithubLogoActive,
   },
   {
     icon: TwitterLogo,
-    link: '/',
+    link: 'https://twitter.com/bdbose123',
+    active: TwitterLogoActive,
   },
   {
     icon: YtLogo,
-    link: '/',
+    link: 'https://www.youtube.com/channel/UC-8SZhRIyLvv2TMHkpghjCQ',
+    active: YtLogoActive,
   },
   {
     icon: FbLogo,
-    link: '/',
+    link: 'https://www.facebook.com/bidipto.bose',
+    active: FbLogoActive,
   },
   {
     icon: InstaLogo,
-    link: '/',
+    link: 'https://www.instagram.com/bdbose/',
+    active: InstaLogoActive,
   },
 ];
 
@@ -215,7 +227,19 @@ const Home = () => {
                 key={ele.link}
                 target='_blank'
                 rel='noreferrer'>
-                <img className='img-icon' src={ele.icon} alt='' />
+                <img
+                  className='img-icon'
+                  onMouseOver={(e) => {
+                    e.target.style.transform = 'scale(1.5)';
+                    e.target.src = ele.active;
+                  }}
+                  onMouseOut={(e) => {
+                    e.target.style.transform = 'scale(1)';
+                    e.target.src = ele.icon;
+                  }}
+                  src={ele.icon}
+                  alt=''
+                />
               </a>
             ))}
           </div>
